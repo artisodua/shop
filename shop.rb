ac749ba63cf6be79692b2b5e98c14bf78b3c6e2f
artisodua <artisodua@gmail.com>
2017-10-20 17:02:52 +0300

    add new class

---
+++ shop.rb
@@ -0,0 +1,18 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative "lib/product"
+require_relative "lib/movie"
+require_relative "lib/book"
+
+array = {price: nil, amount: nil}
+
+movie = Movie.new(price: 390, amount: 4)
+
+puts "Фильм Леон стоит #{movie.price} руб."
